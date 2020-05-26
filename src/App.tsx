ff7cb5c86a6a1f427ec149e8dcd73df9193387e2
gkubiak92 <gkubiak92@gmail.com>
2020-05-26 20:02:43 +0200

    Add blank homepage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/header/header-component";
 import "./App.css";
+import HomePage from "./pages/home/homepage.component";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/home/homepage.component.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const HomePage = () => <main className="homepage"></main>;
+
+export default HomePage;
