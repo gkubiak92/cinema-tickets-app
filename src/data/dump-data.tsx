b11e113c11f2e1765b03ca4e4e1453b82fa92427
Grzegorz Kubiak <gkubiak92@gmail.com>
2020-05-30 15:44:08 +0200

    remove rating styles scss file

--- src/data/dump-data.tsx
+++ src/data/dump-data.tsx
@@ -1,20 +1,3 @@
-import React from "react";
-
-export const tabsData = [
-  {
-    name: "new",
-    content: <div>new</div>,
-  },
-  {
-    name: "popular",
-    content: <div>popular</div>,
-  },
-  {
-    name: "upcoming",
-    content: <div>upcoming</div>,
-  },
-];
-
 export const slides = [
   "https://cdn.pastemagazine.com/www/system/images/photo_albums/best-movie-posters-2016/large/moonlight-ver2-xlg.jpg?1384968217",
   "https://www.joblo.com/assets/images/joblo/posters/2019/01/IO-poster-1.jpg",
