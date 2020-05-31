cd7b39528db6320d2e5c7eed4359e807bdf322bd
Grzegorz Kubiak <gkubiak92@gmail.com>
2020-05-31 19:02:01 +0200

    Add sidedrawer component

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 import "./header.styles.scss";
 import MenuIcon from "../menu-icon/menu-icon.component";
 import SearchIcon from "../search-icon/search-icon.component";
+import SideDrawer from "../side-drawer/sidedrawer.component";
 
 const Header = () => (
   <header className="header">
+    <SideDrawer show={false} />
     <Link to="/menu">
       <MenuIcon />
     </Link>
