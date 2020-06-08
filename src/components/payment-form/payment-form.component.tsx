a0a200ea6a1e4815f2645bb034e4608d398d6302
gkubiak92 <gkubiak92@gmail.com>
2020-06-08 22:09:54 +0200

    Add payment form validation for all fields

--- src/components/payment-form/payment-form.component.tsx
+++ src/components/payment-form/payment-form.component.tsx
@@ -1,24 +1,82 @@
 import React, { useState, FormEvent, ChangeEvent } from "react";
 import "./paymentform.styles.scss";
 import CustomButton from "components/custom-button/custombutton.component";
+import { IFormErrorMsgs } from "./types";
+import { validateEmail, validateName } from "./utils";
 
 const PaymentForm = () => {
   const [formData, setFormData] = useState({
     email: "",
     firstName: "",
     lastName: "",
+    errors: {
+      email: "",
+      firstName: "",
+      lastName: "",
+    },
+    isFormValid: false,
   });
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setFormData({ email: "", firstName: "", lastName: "" });
+    setFormData({
+      email: "",
+      firstName: "",
+      lastName: "",
+      errors: { email: "", firstName: "", lastName: "" } as IFormErrorMsgs,
+      isFormValid: false,
+    });
   };
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target as HTMLInputElement;
+    const errors = validateInput(e);
+    const isFormValid = validateForm(errors);
+
     setFormData({
       ...formData,
       [name]: value,
+      errors,
+      isFormValid,
     });
+  };
+
+  const validateInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const errors: IFormErrorMsgs = { ...formData.errors };
+    const { name, value } = e.target as HTMLInputElement;
+
+    switch (name) {
+      case "email":
+        if (!value) {
+          errors.email = "Field cannot be empty";
+        } else {
+          errors.email = validateEmail(value)
+            ? ""
+            : "Enter proper e-mail address";
+        }
+        break;
+      case "firstName":
+      case "lastName":
+        if (!value) {
+          errors[name] = "Field cannot be empty";
+        } else {
+          errors[name] = validateName(value) ? "" : "Enter only letters";
+        }
+        break;
+      default:
+        break;
+    }
+
+    return errors;
+  };
+
+  const validateForm = (errors: IFormErrorMsgs) => {
+    for (let [, value] of Object.entries(errors)) {
+      if (value !== "") {
+        return false;
+      }
+    }
+
+    return true;
   };
 
   return (
@@ -30,7 +88,9 @@ const PaymentForm = () => {
         id="email"
         value={formData.email}
         onChange={handleInputChange}
+        required
       />
+      <p className="errorMsg">{formData.errors.email}</p>
       <label htmlFor="firstname">First name</label>
       <input
         type="text"
@@ -38,7 +98,9 @@ const PaymentForm = () => {
         id="firstName"
         value={formData.firstName}
         onChange={handleInputChange}
+        required
       />
+      <p className="errorMsg">{formData.errors.firstName}</p>
       <label htmlFor="lastname">Last name</label>
       <input
         type="text"
@@ -46,8 +108,11 @@ const PaymentForm = () => {
         id="lastName"
         value={formData.lastName}
         onChange={handleInputChange}
+        required
       />
-      <CustomButton type="submit" block>
+      <p className="errorMsg">{formData.errors.lastName}</p>
+
+      <CustomButton type="submit" block disabled={!formData.isFormValid}>
         Confirm payment
       </CustomButton>
     </form>
---
+++ src/components/payment-form/types.ts
@@ -0,0 +1,5 @@
+export interface IFormErrorMsgs {
+  email: string;
+  firstName: string;
+  lastName: string;
+}
