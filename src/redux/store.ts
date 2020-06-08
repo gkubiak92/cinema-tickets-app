b0ca81a5040e70c621808101fbad06a0f6a0b69e
gkubiak92 <gkubiak92@gmail.com>
2020-06-08 19:08:03 +0200

    Added end configured redux-persist library for persistent store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
-import rootReducer from "./root-reducer";
+import { persistStore } from "redux-persist";
+import persistedRootReducer from "./root-reducer";
 import logger from "redux-logger";
 
 const middlewares = [];
@@ -8,4 +9,9 @@ if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(
+  persistedRootReducer,
+  applyMiddleware(...middlewares)
+);
+
+export const persistor = persistStore(store);
