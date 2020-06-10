539728bea20978acc95b541bdc126dbe7c6add16
gkubiak92 <gkubiak92@gmail.com>
2020-06-10 19:30:01 +0200

    Add .env to gitignore

--- src/pages/home/homepage.component.tsx
+++ src/pages/home/homepage.component.tsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 import Tabs from "../../components/tabs/tabs.component";
 import Slider from "../../components/slider/slider.component";
-import { slides } from "../../data/dump-data";
+import { slides, movies } from "../../data/dump-data";
 import MoviesList from "../../components/movies-list/movies-list.component";
-import { selectNewMovies, selectPopularMovies, selectUpcomingMovies } from "../../redux/movies/movies.selectors";
-import { createStructuredSelector } from 'reselect'
+import {
+  selectNewMovies,
+  selectPopularMovies,
+  selectUpcomingMovies,
+} from "../../redux/movies/movies.selectors";
+import { createStructuredSelector } from "reselect";
 import { IHomePageProps } from "./types";
 import { IRootState } from "../../redux/types";
+import { addCollectionAndDocuments } from "firebase/firebase.utils";
 
-const HomePage = ({ newMovies, popularMovies, upcomingMovies }: IHomePageProps) => {
+const HomePage = ({
+  newMovies,
+  popularMovies,
+  upcomingMovies,
+}: IHomePageProps) => {
   const tabsData = [
     {
       name: "new",
@@ -28,6 +37,9 @@ const HomePage = ({ newMovies, popularMovies, upcomingMovies }: IHomePageProps) 
   return (
     <main className="homepage">
       <Slider interval={3000} slides={slides} />
+      <button onClick={() => addCollectionAndDocuments("movies", movies)}>
+        UPLOAD DUMP DATA
+      </button>
       <Tabs tabs={tabsData} />
     </main>
   );
@@ -36,7 +48,7 @@ const HomePage = ({ newMovies, popularMovies, upcomingMovies }: IHomePageProps) 
 const mapStateToProps = createStructuredSelector<IRootState, IHomePageProps>({
   newMovies: selectNewMovies,
   popularMovies: selectPopularMovies,
-  upcomingMovies: selectUpcomingMovies
+  upcomingMovies: selectUpcomingMovies,
 });
 
 export default connect(mapStateToProps)(HomePage);
